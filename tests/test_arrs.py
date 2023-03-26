a01fe12e8713868758f0106962fc32c4fef24a0e
davyd <davydow.val@yandex.ru>
2023-03-26 23:20:39 +0700

    Revert "the tests pass"
    This reverts commit a42448f7042b1c1594a952ff0ffccd394fe6b8db.

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,17 +2,10 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], 1, "test") == 3
     assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([7, 8, 9], 0) != 8
-    assert arrs.get([1, 1, 1, 1], 0, "nothing there") in [1, 2]
-    assert arrs.get(["one", "two", "three"], 2, "this is not str") == "three"
-    assert arrs.get(["one", 2, False, {1: "nothing"}], 1) == 2
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([5, 6, 7, 8], 1) == [6, 7, 8]
-    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
-    assert arrs.my_slice([1], 0) == [1]
