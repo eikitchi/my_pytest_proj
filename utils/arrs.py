9306485c4a9b1a71bdacee4133929098a84af9c2
davyd <davydow.val@yandex.ru>
2023-03-27 19:01:30 +0700

    the tests pass

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,10 +11,11 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
-        return default
+    if 0 <= index < len(array):
+        return array[index]
+    return default
+#[1,2,3]
 
-    return array[index]
 
 
 def my_slice(coll, start=0, end=None):
