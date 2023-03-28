7df188ebc52d50d3230e8997934bb090c4478bf7
davyd <davydow.val@yandex.ru>
2023-03-28 17:06:51 +0700

    the tests pass

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,13 @@
 from utils import arrs
-import unittest
 
 
-class TestCalc(unittest.TestCase):
+def test_get():
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], 0, "test") == "test"
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
-
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -5), [1, 2, 3])
-        self.assertEqual(arrs.my_slice([], 1), [])
+def test_slice():
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
+    assert arrs.my_slice([], 1) == []
